67fb863dbcbf25c5ffacc8de4d8160b8d894a6be
nia-berri <db5893a@america.edu>
2026-04-06 00:25:21 +0100

    loading libraries, data and

--- LoadCleanData.R
+++ LoadCleanData.R
@@ -1,1 +1,16 @@
 #Loading and Cleaning Data for Project 1
+
+##Loading libraries
+library(tidyverse)
+library(janitor)
+library(ggplot2)
+
+##Loading the Data
+unemployment <- read_csv("unemployment.csv")
+
+summary(unemployment)
+names(unemployment)
+view(unemployment)
+
+unemployment <- unemployment |>
+  
